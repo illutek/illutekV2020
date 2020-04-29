4839f74520ec5b58d48cf6414d6a0030d6dbc255
illutek <stefanvandenborne@gmail.com>
2020-04-29 10:12:17 +0200

    add close option to modal

--- js/link-modal.js
+++ js/link-modal.js
@@ -10,12 +10,18 @@
   var closeModal = document.querySelector('.c-close-btn');
   var overlayModal = document.querySelector('.n-links__modal-bcg--hidden');
 
-  // console.log(showModal);
+function hiddenModal () {
+  overlayModal.classList.remove('n-links__modal-bcg--visible');
+  overlayModal.classList.add('n-links__modal-bcg--hidden');
+}
 
   function showModal() {
-    overlayModal.classList.toggle('n-links__modal-bcg--visible');
+    overlayModal.classList.add('n-links__modal-bcg--visible');
+    overlayModal.classList.remove('n-links__modal-bcg--hidden');
   }
 
   linkModal.addEventListener('click', showModal);
+
+  closeModal.addEventListener('click', hiddenModal);
 
 })();
